f968372b8bdd0951cb5d191b9fe29687aa9bd090
Brendan Gowen <bgowengaming1@gmail.com>
2017-01-17 18:21:20 -0500

    Math now actually works finally

--- example.cpp
+++ example.cpp
@@ -48,31 +48,31 @@ int main(void)
 	else if(operation == "cubicroot" | operation == "Cubicroot" | operation == "cubicRoot" | operation == "CubicRoot")
 	{
 		double num1 = io::dblinput("Please enter a number.");
-		answer == math::cubicroot(num1);
+		answer = math::cubicroot(num1);
 	}
 	
 	else if (operation == "roundup" | operation == "Roundup" | operation == "roundUp" | operation == "RoundUp")
 	{
 		double num1 = io::dblinput("Please enter a number.");
-		answer == math::roundup(num1);
+		answer = math::roundup(num1);
 	}
 	
 	else if(operation == "rounddown" | operation == "Rounddown" | operation == "roundDown" | operation == "RoundDown")
 	{
 		double num1 = io::dblinput("Please enter a number.");
-		answer == math::rounddown(num1);
+		answer = math::rounddown(num1);
+	}
+
+	else if(operation == "power" | operation == "Power")
+	{
+		double num1 = io::dblinput("Please enter a number.");
+		double num2 = io::dblinput("Please enter a numner.");
+		answer = math::power(num1, num2);
 	}
 	
 	else if(operation == "pi" | operation == "Pi")
 	{
 		answer = math::pi();
-	}
-	
-	else if(operation == "power" | operation == "Power")
-	{
-		double num1 = io::dblinput("Please enter a number.");
-		double num2 = io::dblinput("Please enter a numner.");
-		answer == math::power(num1, num2);
 	}
 	
 	cout << answer << endl;
