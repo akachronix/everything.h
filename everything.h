1dd08ddf273320fcbb6de667985be48523c13c0c
Brendan Gowen <bgowengaming1@gmail.com>
2017-01-18 14:54:52 -0500

    Update everything.h

--- everything.h
+++ everything.h
@@ -1,5 +1,11 @@
+/* Name:   		everything.h
+   Author:      Brendan Gowen 
+   Description: A header file that
+   includes everything I might need
+   for developing an application. */
+
 #ifndef EVERYTHING_H
-#define EVERYTHING_H
+#define EVERYTHING_H  
 
 #include <iostream>
 #include <unistd.h>
