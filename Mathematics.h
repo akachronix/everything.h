03eebdb949ae6016241a03bb4734d79d33357aab
Brendan Gowen <bgowengaming1@gmail.com>
2017-09-01 15:15:21 -0400

    had to fix indentation because github's tabbing is actual aids

--- Mathematics.h
+++ Mathematics.h
@@ -6,19 +6,19 @@
 #define multiply (a, b)	(a * b)
 #define divide   (a, b) (a / b)
 
-typedef struct{float length, width, area;}			rectangle_t;
-typedef struct{float height, side1, side2, area;}	trapezoid_t;
-typedef struct{float base, height, area;}			triangle_t;
-typedef struct{float radius, area;} 				circle_t;
-typedef struct{float side, area;}					square_t;
+typedef struct{float length, width, area;} rectangle_t;
+typedef struct{float height, side1, side2, area;} trapezoid_t;
+typedef struct{float base, height, area;} triangle_t;
+typedef struct{float radius, area;} circle_t;
+typedef struct{float side, area;} square_t;
 
 long double factorial(long double limit);
 long double power(long double base, long double exponent);
 
 rectangle_t newRectangle(float initlength, float initwidth);
 trapezoid_t newTrapezoid(float initheight, float initside1, float initside2);
-triangle_t	newTriangle(float initbase, float initheight);
-circle_t	newCircle(float initradius);
-square_t	newSquare(float initside);
+triangle_t newTriangle(float initbase, float initheight);
+circle_t newCircle(float initradius);
+square_t newSquare(float initside);
 
 #endif
