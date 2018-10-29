4b6e4462812ad98c86cafcbf7b9f9cd2960b6991
Brendan Gowen <bgowengaming1@gmail.com>
2018-10-28 22:19:00 -0400

    seperate class definition and function declarations

--- include/libvector.hpp
+++ include/libvector.hpp
@@ -1,75 +1,10 @@
 #pragma once
 
 #include <initializer_list>
+#include <stdexcept>
 #include <iterator>
 #include <utility>
 #include <new>
-
-#include <exception>
-
-class OutOfRange : public std::exception
-{
-private:
-	const char* m_msg;
-	const unsigned int m_index;
-	const unsigned int m_size;
-
-public:
-	OutOfRange(const char* msg, const unsigned int index, const unsigned int size)
-		: m_msg(msg), m_index(index), m_size(size)
-	{
-		
-	}
-
-	const char* what() const throw()
-	{
-		return "OutOfRange";
-	}
-
-	const char* msg() const noexcept
-	{
-		return m_msg;
-	}
-
-	const unsigned int index() const noexcept
-	{
-		return m_index;
-	}
-
-	const unsigned int size() const noexcept
-	{
-		return m_size;
-	}
-};
-
-class BadAlloc : public std::exception
-{
-private:
-	const char* m_msg;
-	const unsigned int m_size;
-
-public:
-	BadAlloc(const char* msg, const unsigned int size)
-		: m_msg(msg), m_size(size)
-	{
-
-	}
-
-	const char* what() const throw()
-	{
-		return "BadAlloc";
-	}
-
-	const char* msg() const noexcept
-	{
-		return m_msg;
-	}
-
-	const unsigned int size() const noexcept
-	{
-		return m_size;
-	}
-};
 
 template<typename T>
 class Vector
@@ -79,212 +14,243 @@ private:
 	T* m_array;
 
 public:
-	Vector()
-		: m_size(0), m_array(nullptr)
-	{
+	Vector();
+	Vector(unsigned int size);
+	Vector(std::initializer_list<T> list);
 
-	}
+	Vector(const Vector& v2);
 
-	Vector(unsigned int size)
-		: m_size(size)
-	{
-		m_array = new(std::nothrow) T[size];
+	~Vector();
 
-		if (m_array == nullptr)
-			throw BadAlloc("Error while allocating memory.", size);
-	}
-	
-	Vector(std::initializer_list<T> list)
-		: Vector(list.size())
-	{
-		int i = 0;
+	// element access functions
+	T& operator[](const unsigned int index) noexcept;
+	T& at(const unsigned int index);
+	T* data() noexcept;
+	T& front() noexcept;
+	T& back() noexcept;
 
-		for (auto& element : list)
-		{
-			m_array[i] = element;
-			++i;
-		}
-	}
+	// size functions
+	unsigned int size() const noexcept;
+	unsigned int bytes() const noexcept;
+	bool empty() const noexcept;
 
-	Vector(const Vector& v2)
-	{
-		m_size = v2.m_size;
-		m_array = new(std::nothrow) T[v2.m_size];
+	// iterator functions
+	T* begin() noexcept;
+	T* end() noexcept;
+	T* cbegin() const noexcept;
+	T* cend() const noexcept;
+	std::reverse_iterator<T*> rbegin() noexcept;
+	std::reverse_iterator<T*> rend() noexcept;
+	std::reverse_iterator<const T*> crbegin() const noexcept;
+	std::reverse_iterator<const T*> crend() const noexcept;
 
-		if (m_array == nullptr)
-			throw BadAlloc("Error while copying memory.", v2.m_size);
-
-		for (unsigned int i = 0; i < v2.m_size; ++i)
-			m_array[i] = v2.m_array[i];
-	}
-
-	~Vector()
-	{
-		if (m_array != nullptr)
-			delete[] m_array;
-	}
-
-	T& operator[](const unsigned int index) noexcept
-	{
-		return m_array[index];
-	}
-	
-	T& at(const unsigned int index)
-	{
-		if (index >= m_size)
-			throw OutOfRange("Index is out of range.", index, m_size);
-		
-		return m_array[index];
-	}
-
-	T* data() noexcept
-	{
-		return m_array;
-	}
-
-	T& front() noexcept
-	{
-		return m_array[0];
-	}
-
-	T& back() noexcept
-	{
-		return m_array[m_size];
-	}
-
-	unsigned int size() const noexcept
-	{
-		return m_size;
-	}
-
-	unsigned int bytes() const noexcept
-	{
-		return m_size * sizeof(T);
-	}
-
-	bool empty() const noexcept
-	{
-		if (m_array == nullptr | m_size == 0)
-			return true;
-		
-		return false;
-	}
-
-	T* begin() noexcept
-	{
-		return &m_array[0];
-	}
-
-	T* end() noexcept
-	{
-		return &m_array[m_size];
-	}
-
-	T* cbegin() const noexcept
-	{
-		return &m_array[0];
-	}
-
-	T* cend() const noexcept
-	{
-		return &m_array[m_size];
-	}
-
-	std::reverse_iterator<T*> rbegin() noexcept
-	{
-		return std::reverse_iterator<T*>(&m_array[m_size]);
-	}
-
-	std::reverse_iterator<T*> rend() noexcept
-	{
-		return std::reverse_iterator<T*>(&m_array[0]);
-	}
-
-	std::reverse_iterator<const T*> crbegin() const noexcept
-	{
-		return std::reverse_iterator<const T*>(&m_array[m_size]);
-	}
-
-	std::reverse_iterator<const T*> crend() const noexcept
-	{
-		return std::reverse_iterator<const T*>(&m_array[0]);
-	}
-
-	void push_back(T new_element)
-	{
-		T* buffer = new(std::nothrow) T[m_size + 1];
-
-		if (buffer == nullptr)
-			throw BadAlloc("Error allocating memory for buffer.", m_size + 1);
-		
-		for (unsigned int i = 0; i < m_size; ++i)
-			buffer[i] = m_array[i];
-		buffer[m_size] = new_element;
-
-		delete[] m_array;
-
-		m_array = new(std::nothrow) T[m_size + 1];
-		
-		if (m_array == nullptr)
-			throw BadAlloc("Error allocating memory for new array.", m_size + 1);
-
-		for (unsigned int i = 0; i < m_size + 1; ++i)
-			m_array[i] = buffer[i];
-
-		delete[] buffer;
-
-		m_size += 1;
-	}
-
-	template<class... Args>
-	void emplace_back(Args&&... args)
-	{
-		T* buffer = new(std::nothrow) T[m_size + 1];
-
-		if (buffer == nullptr)
-			throw BadAlloc("Error allocating memory for buffer.", m_size + 1);
-		
-		for (unsigned int i = 0; i < m_size; ++i)
-			buffer[i] = m_array[i];
-		buffer[m_size] = std::move( T(std::forward<Args>(args)...) );
-
-		delete[] m_array;
-
-		m_array = new(std::nothrow) T[m_size + 1];
-		
-		if (m_array == nullptr)
-			throw BadAlloc("Error allocating memory for new array.", m_size + 1);
-
-		for (unsigned int i = 0; i < m_size + 1; ++i)
-			m_array[i] = buffer[i];
-
-		delete[] buffer;
-
-		m_size += 1;
-	}
-
-	void pop_back()
-	{
-		T* buffer = new(std::nothrow) T[m_size - 1];
-
-		if (buffer == nullptr)
-			throw BadAlloc("Error allocating memory for buffer.", m_size - 1);
-
-		for (unsigned int i = 0; i < (m_size - 1); ++i)
-			buffer[i] = m_array[i];
-		
-		delete[] m_array;
-
-		m_array = new(std::nothrow) T[m_size - 1];
-		
-		if (m_array == nullptr)
-			throw BadAlloc("Error allocating memory for new array.", m_size - 1);
-
-		for (unsigned int i = 0; i < (m_size - 1); ++i)
-			m_array[i] = buffer[i];
-		
-		delete[] buffer;
-
-		m_size -= 1;
-	}
+	// element modifying functions
+	template<class... Args> void emplace_back(Args&&... args);
+	void push_back(T new_element);
+	void pop_back();
 };
+
+template<typename T>
+Vector<T>::Vector()
+	: m_size(0), m_array(nullptr)
+{
+
+}
+
+template<typename T>
+Vector<T>::Vector(unsigned int size)
+	: m_size(size)
+{
+	m_array = new T[size];
+}
+
+template<typename T>
+Vector<T>::Vector(std::initializer_list<T> list)
+	: Vector(list.size())
+{
+	int i = 0;
+
+	for (auto& element : list)
+	{
+		m_array[i] = element;
+		++i;
+	}
+}
+
+template<typename T>
+Vector<T>::Vector(const Vector& v2)
+{
+	m_size = v2.m_size;
+	m_array = new T[v2.m_size];
+
+	for (unsigned int i = 0; i < v2.m_size; ++i)
+		m_array[i] = v2.m_array[i];
+}
+
+template<typename T>
+Vector<T>::~Vector()
+{
+	if (m_array != nullptr)
+		delete[] m_array;
+}
+
+template<typename T>
+T& Vector<T>::operator[](const unsigned int index) noexcept
+{
+	return m_array[index];
+}
+
+template<typename T>
+T& Vector<T>::at(const unsigned int index)
+{
+	if (index >= m_size)
+		throw std::out_of_range("vector::at(const unsigned int)");
+
+	return m_array[index];
+}
+
+template<typename T>
+T* Vector<T>::data() noexcept
+{
+	return m_array;
+}
+
+template<typename T>
+T& Vector<T>::front() noexcept
+{
+	return m_array[0];
+}
+
+template<typename T>
+T& Vector<T>::back() noexcept
+{
+	return m_array[m_size];
+}
+
+template<typename T>
+unsigned int Vector<T>::size() const noexcept
+{
+	return m_size;
+}
+
+template<typename T>
+unsigned int Vector<T>::bytes() const noexcept
+{
+	return m_size * sizeof(T);
+}
+
+template<typename T>
+bool Vector<T>::empty() const noexcept
+{
+	if (m_array == nullptr | m_size == 0)
+		return true;
+
+	return false;
+}
+
+template<typename T>
+T* Vector<T>::begin() noexcept
+{
+	return &m_array[0];
+}
+
+template<typename T>
+T* Vector<T>::end() noexcept
+{
+	return &m_array[m_size];
+}
+
+template<typename T>
+T* Vector<T>::cbegin() const noexcept
+{
+	return &m_array[0];
+}
+
+template<typename T>
+T* Vector<T>::cend() const noexcept
+{
+	return &m_array[m_size];
+}
+
+template<typename T>
+std::reverse_iterator<T*> Vector<T>::rbegin() noexcept
+{
+	return std::reverse_iterator<T*>(&m_array[m_size]);
+}
+
+template<typename T>
+std::reverse_iterator<T*> Vector<T>::rend() noexcept
+{
+	return std::reverse_iterator<T*>(&m_array[0]);
+}
+
+template<typename T>
+std::reverse_iterator<const T*> Vector<T>::crbegin() const noexcept
+{
+	return std::reverse_iterator<const T*>(&m_array[m_size]);
+}
+
+template<typename T>
+std::reverse_iterator<const T*> Vector<T>::crend() const noexcept
+{
+	return std::reverse_iterator<const T*>(&m_array[0]);
+}
+
+template<typename T>
+template<class... Args>
+void Vector<T>::emplace_back(Args&&... args)
+{
+	T* buffer = new T[m_size + 1];
+
+	for (unsigned int i = 0; i < m_size; ++i)
+		buffer[i] = m_array[i];
+	buffer[m_size] = std::move(T(std::forward<Args>(args)...));
+
+	delete[] m_array;
+
+	m_array = new T[m_size + 1];
+
+	for (unsigned int i = 0; i < m_size + 1; ++i)
+		m_array[i] = buffer[i];
+
+	delete[] buffer;
+
+	m_size += 1;
+}
+
+template<typename T>
+void Vector<T>::push_back(T new_element)
+{
+	T* buffer = new T[m_size + 1];
+
+	for (unsigned int i = 0; i < m_size; ++i)
+		buffer[i] = m_array[i];
+	buffer[m_size] = new_element;
+
+	delete[] m_array;
+
+	for (unsigned int i = 0; i < m_size + 1; ++i)
+		m_array[i] = buffer[i];
+
+	delete[] buffer;
+
+	m_size += 1;
+}
+
+template<typename T>
+void Vector<T>::pop_back()
+{
+	T* buffer = new T[m_size - 1];
+
+	for (unsigned int i = 0; i < (m_size - 1); ++i)
+		buffer[i] = m_array[i];
+
+	delete[] m_array;
+
+	for (unsigned int i = 0; i < (m_size - 1); ++i)
+		m_array[i] = buffer[i];
+
+	delete[] buffer;
+
+	m_size -= 1;
+}
