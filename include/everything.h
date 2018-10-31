b03f7a5c7f53ee1d60ae70f4f9da6a53b65b93c1
Brendan Gowen <bgowengaming1@gmail.com>
2018-10-31 00:38:54 -0400

    updated everything.h

--- include/everything.h
+++ include/everything.h
@@ -1,13 +1,9 @@
 #pragma once
 
 #ifdef __cplusplus
-    #include "libvector.hpp"
-    #include "libinput.hpp"
-    #include "libmath.hpp"
-    #include "libpair.hpp"
-    #include "libcalc.hpp"
-    #include "liblog.hpp"
-    #include "libptr.hpp"
+	#include "vector.hpp"
+	#include "math.hpp"
+	#include "log.hpp"
 #else
     #error everything.h is only for C++. Sorry!
 #endif
