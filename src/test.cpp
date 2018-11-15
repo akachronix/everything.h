4a6d500ea2a0a0f93c694cd8cef053ab7c5fd162
Brendan Gowen <bgowengaming1@gmail.com>
2018-11-15 12:50:24 -0500

    updated test program

--- src/test.cpp
+++ src/test.cpp
@@ -1,15 +1,17 @@
 #include <iostream>
+#include <type_traits>
 
-#include "array.hpp"
+#include "brisk.h"
 
 int main(void)
 {
 	brisk::array<int, 50> arr;
-	for (int i = 0; i < arr.size(); ++i) {
+	for (int i = 0; i < arr.size(); ++i)
 		arr[i] = i;
-
-		std::cout << arr[i] << ' ';
-	}
+	
+	brisk::vector<std::remove_reference<decltype(arr[0])>::type> vector = brisk::copy(arr);
+	for (auto& it : vector)
+		std::cout << it << ' ';
 
 	return 0;
 }
