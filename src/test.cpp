65e3e6fea437528d9ca58f886a925f81000af704
Brendan Gowen <bgowengaming1@gmail.com>
2018-11-15 11:42:34 -0500

    Add files via upload

--- src/test.cpp
+++ src/test.cpp
@@ -1,26 +1,15 @@
 #include <iostream>
-#include <string>
 
-#include "everything.h"
+#include "array.hpp"
 
 int main(void)
 {
-	Logger console(loglevel_t::everything, "console.log");
+	brisk::array<int, 50> arr;
+	for (int i = 0; i < arr.size(); ++i) {
+		arr[i] = i;
 
-	Vector<int> vector = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-	vector.shrink_to_fit();
+		std::cout << arr[i] << ' ';
+	}
 
-	console << "Elements allocated: " << vector.capacity() << newl;
-	console << "Elements allocated (bytes): " << vector.capacity_bytes() << newl;
-	console << newl;
-	console << "Elements filled: " << vector.size() << newl;
-	console << "Elements filled (bytes): " << vector.size_bytes() << newl;
-	console << newl;
-	console << "Front element: " << vector.front() << newl;
-	console << "Back element: " << vector.back() << newl;
-	console << newl;
-	console << "Empty? " << (vector.empty() ? "yes" : "no") << newl;
-
-	std::cin.get();
 	return 0;
 }
