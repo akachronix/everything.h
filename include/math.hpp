3ac4f9f6fc6604bd5000537b6956952c614d4fa1
Brendan Gowen <bgowengaming1@gmail.com>
2018-12-03 11:22:33 -0500

    change class to struct and move funcs inside class declaration

--- include/math.hpp
+++ include/math.hpp
@@ -7,354 +7,211 @@ namespace brisk
 	const long double pi = 3.14159265358979323846;
 
 	template <class T = long double>
-	class trapezoid
+	struct trapezoid
 	{
-	public:
 		T side1, side2, height;
 
 		trapezoid() = default;
-		trapezoid(T _side1, T _side2, T _height);
 
-		T area();
-		T operator()();
+		trapezoid(T _side1, T _side2, T _height)
+			: side1(_side1), side2(_side2), height(_height)
+		{
+
+		}
+
+		T area()
+		{
+			return (side1 + side2) / 2 * height;
+		}
 	};
 
 	template <class T = long double>
-	class rectangle
+	struct rectangle
 	{
-	public:
 		T length, width;
 
 		rectangle() = default;
-		rectangle(T _length, T _width);
 
-		T area();
-		T operator()();
+		rectangle(T _length, T _width)
+			: length(_length), width(_width)
+		{
+
+		}
+
+		T area()
+		{
+			return length * width;
+		}
 	};
 
 	template <class T = long double>
-	class triangle
+	struct triangle
 	{
-	public:
 		T base, height;
 
 		triangle() = default;
-		triangle(T _base, T _height);
 
-		T area();
-		T operator()();
+		triangle(T _base, T _height)
+			: base(_base), height(_height)
+		{
+
+		}
+
+		T area()
+		{
+			return (base * height) / 2;
+		}
 	};
 
 	template <class T = long double>
-	class rhombus
+	struct rhombus
 	{
-	public:
 		T side;
 
 		rhombus() = default;
-		rhombus(T _side);
 
-		T area();
-		T operator()();
+		rhombus(T _side)
+			: side(_side)
+		{
+
+		}
+
+		T area()
+		{
+			return side / 2;
+		}
 	};
 
 	template <class T = long double>
-	class hexagon
+	struct hexagon
 	{
-	public:
 		T side;
 
 		hexagon() = default;
-		hexagon(T _side);
 
-		T area();
-		T operator()();
+		hexagon(T _side)
+			: side(_side)
+		{
+
+		}
+
+		T area()
+		{
+			return 3 * sqrt(3) / 2 * (side * side);
+		}
 	};
 
 	template <class T = long double>
-	class circle
+	struct circle
 	{
-	public:
 		T radius;
 
 		circle() = default;
-		circle(T _radius);
 
-		T area();
-		T operator()();
+		circle(T _radius)
+			: radius(_radius)
+		{
+
+		}
+
+		T area()
+		{
+			return pi * radius * radius;
+		}
 	};
 
 	template <class T = long double>
-	class square
+	struct square
 	{
-	public:
 		T side;
 
 		square() = default;
-		square(T _side);
 
-		T area();
-		T operator()();
+		square(T _side)
+			: side(_side)
+		{
+
+		}
+
+		T area()
+		{
+			return side * side;
+		}
 	};
 
 	template <class T = long double>
-	class rectangular_prism
+	struct rectangular_prism
 	{
-	public:
 		T length, width, height;
 
 		rectangular_prism() = default;
-		rectangular_prism(T _length, T _width, T _height);
 
-		T area();
-		T operator()();
+		rectangular_prism(T _length, T _width, T _height)
+			: length(_length), width(_width), height(_height)
+		{
+
+		}
+
+		T area()
+		{
+			return length * width * height;
+		}
 	};
 
 	template <class T = long double>
-	class triangular_prism
+	struct triangular_prism
 	{
-	public:
 		T length, width, height;
 
 		triangular_prism() = default;
-		triangular_prism(T _length, T _width, T _height);
 
-		T area();
-		T operator()();
+		triangular_prism(T _length, T _width, T _height)
+			: length(_length), width(_width), height(_height)
+		{
+
+		}
+
+		T area()
+		{
+			return (length * width * height) / 2;
+		}
 	};
 
 	template <class T = long double>
-	class cone
+	struct cone
 	{
-	public:
 		T radius, height;
 
 		cone() = default;
-		cone(T _radius, T _height);
 
-		T area();
-		T operator()();
+		cone(T _radius, T _height)
+			: radius(_radius), height(_height)
+		{
+
+		}
+
+		T area()
+		{
+			return pi * (radius * radius) * (height / 3);
+		}
 	};
 
 	template <class T = long double>
-	class cube
+	struct cube
 	{
-	public:
 		T face;
 
 		cube() = default;
-		cube(T _face);
 
-		T area();
-		T operator()();
+		cube(T _face)
+			: face(_face)
+		{
+
+		}
+
+		T area()
+		{
+			return face * face * face;
+		}
 	};
-
-	template <class T>
-	trapezoid<T>::trapezoid(T _side1, T _side2, T _height)
-		: side1(_side1), side2(_side2), height(_height)
-	{
-
-	}
-
-	template <class T>
-	T trapezoid<T>::area()
-	{
-		return (side1 + side2) / 2 * height;
-	}
-
-	template <class T>
-	T trapezoid<T>::operator()()
-	{
-		return area();
-	}
-
-	template <class T>
-	rectangle<T>::rectangle(T _length, T _width)
-		: length(_length), width(_width)
-	{
-	
-	}
-
-	template <class T>
-	T rectangle<T>::area()
-	{
-		return length * width;
-	}
-
-	template <class T>
-	T rectangle<T>::operator()()
-	{
-		return area();
-	}
-
-	template <class T>
-	triangle<T>::triangle(T _base, T _height)
-		: base(_base), height(_height)
-	{
-
-	}
-
-	template <class T>
-	T triangle<T>::area()
-	{
-		return (base * height) / 2;
-	}
-
-	template <class T>
-	T triangle<T>::operator()()
-	{
-		return area();
-	}
-
-	template <class T>
-	rhombus<T>::rhombus(T _side)
-		: side(_side)
-	{
-
-	}
-
-	template <class T>
-	T rhombus<T>::area()
-	{
-		return side / 2;
-	}
-
-	template <class T>
-	T rhombus<T>::operator()()
-	{
-		return area();
-	}
-
-	template <class T>
-	hexagon<T>::hexagon(T _side)
-		: side(_side)
-	{
-
-	}
-
-	template <class T>
-	T hexagon<T>::area()
-	{
-		return 3 * sqrt(3) / 2 * (side * side);
-	}
-
-	template <class T>
-	T hexagon<T>::operator()()
-	{
-		return area();
-	}
-
-	template <class T>
-	circle<T>::circle(T _radius)
-		: radius(_radius)
-	{
-
-	}
-
-	template <class T>
-	T circle<T>::area()
-	{
-		return (radius * radius) * pi;
-	}
-
-	template <class T>
-	T circle<T>::operator()()
-	{
-		return area();
-	}
-
-	template <class T>
-	square<T>::square(T _side)
-		: side(_side)
-	{
-
-	}
-
-	template <class T>
-	T square<T>::area()
-	{
-		return side * side;
-	}
-
-	template <class T>
-	T square<T>::operator()()
-	{
-		return area();
-	}
-
-	template <class T>
-	rectangular_prism<T>::rectangular_prism(T _length, T _width, T _height)
-		: length(_length), width(_width), height(_height)
-	{
-
-	}
-
-	template <class T>
-	T rectangular_prism<T>::area()
-	{
-		return length * width * height;
-	}
-
-	template <class T>
-	T rectangular_prism<T>::operator()()
-	{
-		return area();
-	}
-
-	template <class T>
-	triangular_prism<T>::triangular_prism(T _length, T _width, T _height)
-		: length(_length), width(_width), height(_height)
-	{
-
-	}
-
-	template <class T>
-	T triangular_prism<T>::area()
-	{
-		return (length * width * height) / 2;
-	}
-
-	template <class T>
-	T triangular_prism<T>::operator()()
-	{
-		return area();
-	}
-
-	template <class T>
-	cone<T>::cone(T _radius, T _height)
-		: radius(_radius), height(_height)
-	{
-
-	}
-
-	template <class T>
-	T cone<T>::area()
-	{
-		return pi * (radius * radius) * (height / 3);
-	}
-
-	template <class T>
-	T cone<T>::operator()()
-	{
-		return area();
-	}
-
-	template <class T>
-	cube<T>::cube(T _face)
-		: face(_face)
-	{
-
-	}
-
-	template <class T>
-	T cube<T>::area()
-	{
-		return face * face * face;
-	}
-
-	template <class T>
-	T cube<T>::operator()()
-	{
-		return area();
-	}
 }
