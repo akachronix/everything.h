12ca7c01cc4cc0f9ba0de3b69f00534f4c2d1847
akachronix <bgowengaming1@gmail.com>
2019-06-27 17:17:18 -0400

    moved headers into brisk folder for better seperation

--- src/test.cpp
+++ src/test.cpp
@@ -1,23 +1,7 @@
-#include "brisk.h"
+#include "brisk/brisk.h"
 
-int main(int argc, const char* argv[])
+int main(void)
 {
-	brisk::logger cout(brisk::loglevel::everything, "cout.log");
-
-	brisk::vector<int> v(10);
-	brisk::fill(v.begin(), v.end(), 10);
-
-	brisk::vector<int> v2(v.size());
-	brisk::copy(v2.begin(), v2.end(), v.begin());
-
-	for (auto& it : v) {
-		cout << it << ' ';
-	}
-
-	for (auto& it : v2) {
-		cout << it << ' ';
-	}
-
-	cout << '\n';
-	cout.shrink_to_fit();
+    brisk::logger cout(brisk::loglevel::everything, "cout.log");
+    cout << "hello, world\n";
 }
